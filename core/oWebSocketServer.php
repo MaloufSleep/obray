3a243f1f558ee62d901eb470a53c1ccd93f8d32f
Nate Obray <nate@maloufsleep.com>
2018-06-21 19:37:10 -0600

    updates to socket service ciphers

--- core/oWebSocketServer.php
+++ core/oWebSocketServer.php
@@ -76,13 +76,14 @@
 
 				$protocol = "ssl";
 				try{
-					
+					exit();
 					$context = 	stream_context_create( array( "ssl" => array( 
 						"verify_peer" => FALSE,
 						"local_cert"=>__WEB_SOCKET_CERT__, 
 						"local_pk"=>__WEB_SOCKET_KEY__, 
 						"passphrase" => __WEB_SOCKET_KEY_PASS__,
-						
+						"ciphers" => "HIGH:!SSLv2:!SSLv3",
+						"disable_compression" => TRUE
 					) ) );
 
 				} catch( Exception $err ){
