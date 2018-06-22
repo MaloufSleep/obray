321ac4600f91d8b6018c08de6fa9b90c1a51b767
Nate Obray <nate@maloufsleep.com>
2018-06-21 21:29:59 -0600

    update ciphers

--- core/oWebSocketServer.php
+++ core/oWebSocketServer.php
@@ -81,7 +81,7 @@
 						"local_cert"=>__WEB_SOCKET_CERT__, 
 						"local_pk"=>__WEB_SOCKET_KEY__, 
 						"passphrase" => __WEB_SOCKET_KEY_PASS__,
-						"ciphers" => "DEFAULT:!SSLv2:!SSLv3",
+						"ciphers" => "DEFAULT",
 						"disable_compression" => TRUE,
 						"SNI_enabled" => TRUE,
 					) ) );
