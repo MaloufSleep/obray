93590d9210f10363cdb1c7e0a735ca07aa088b9f
James Funk <piurafunk@gmail.com>
2021-09-08 15:22:40 -0600

    Testing OObject::getStackTrace()

---
+++ tests/Object/GetStackTraceTest.php
@@ -0,0 +1,49 @@
+<?php
+
+namespace tests\Object;
+
+use Exception;
+use tests\TestCase;
+
+/**
+ * @covers OObject::getStackTrace
+ */
+class GetStackTraceTest extends TestCase
+{
+	public function testGetStackTraceBasic()
+	{
+		$this->assertIsString($this->router->getStackTrace(new Exception()));
+	}
+
+	public function provideExceptionArguments(): array
+	{
+		return [
+			['Hello!'],
+			[null],
+			[fopen('php://memory', 'w')],
+			[1],
+		];
+	}
+
+	/**
+	 * @param $arg
+	 * @dataProvider provideExceptionArguments
+	 */
+	public function testGetStackTraceWithArgument($arg)
+	{
+		try {
+			throwExceptionWithArgument($arg);
+		} catch (Exception $e) {
+			$this->assertIsString($this->router->getStackTrace($e));
+		}
+	}
+}
+
+/**
+ * @param mixed $arg
+ * @throws \Exception
+ */
+function throwExceptionWithArgument($arg)
+{
+	throw new Exception;
+}
