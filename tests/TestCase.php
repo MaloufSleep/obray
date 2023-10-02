463ead6de2decada3cf6bec3af5755bf92f8afac
Cameron Bird <neobird6@gmail.com>
2023-10-02 14:24:20 -0600

    Extracted & wrapped constant definitions to ensure we set them once

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -8,77 +8,107 @@ use Symfony\Component\Console\Output\NullOutput;
 
 class TestCase extends \PHPUnit\Framework\TestCase
 {
-	use FakesAuthentication;
+    use FakesAuthentication;
 
-	/**
-	 * @var \OObject
-	 */
-	protected $router;
+    /**
+     * @var \OObject
+     */
+    protected $router;
 
-	protected function setUp(): void
-	{
-		parent::setUp();
+    public static function setUpBeforeClass(): void
+    {
+        parent::setUpBeforeClass();
 
-		global $_SESSION;
-		$_SESSION = [];
-		unset($_SERVER['PHP_AUTH_USER']);
-		unset($_SERVER['PHP_AUTH_PW']);
+        global $_SESSION;
+        $_SESSION = [];
+        unset($_SERVER['PHP_AUTH_USER']);
+        unset($_SERVER['PHP_AUTH_PW']);
 
-		$this->router = new OObject();
-		$this->router->setOutput(new NullOutput());
-		$this->router::setContainerSingleton(null);
-
-		define('__OBRAY_ROUTES__', serialize([
-			'obray' => realpath(__DIR__ . '/../core') . '/',
-			'app' => realpath(__DIR__ . '/../test_files') . '/',
-			'm' => realpath(__DIR__ . '/../test_files/models') . '/',
-		]));
-		define('__OBRAY_SITE_ROOT__', realpath(__DIR__ . '/../test_files') . '/');
-		define('__OBRAY_NAMESPACE_ROOT__', realpath(__DIR__ . '/../test_files') . '/');
-		define('__OBRAY_APP_NAME__', 'App');
-		define('__APP__', 'Obray');
-		define('__OBRAY_DATABASE_HOST__', 'mysql');
-		define('__OBRAY_DATABASE_NAME__', 'obray');
-		define('__OBRAY_DATABASE_USERNAME__', 'root');
-		define('__OBRAY_DATABASE_PASSWORD__', 'root');
-
-		define('__LOGS__', realpath(__DIR__ . '/../logs') . '/');
+        if (!defined('__OBRAY_ROUTES__')) {
+            define(
+                '__OBRAY_ROUTES__',
+                serialize([
+                    'obray' => realpath(__DIR__ . '/../core') . '/',
+                    'app' => realpath(__DIR__ . '/../test_files') . '/',
+                    'm' => realpath(__DIR__ . '/../test_files/models') . '/',
+                ])
+            );
+        }
+        if (!defined('__OBRAY_SITE_ROOT__')) {
+            define('__OBRAY_SITE_ROOT__', realpath(__DIR__ . '/../test_files') . '/');
+        }
+        if (!defined('__OBRAY_NAMESPACE_ROOT__')) {
+            define('__OBRAY_NAMESPACE_ROOT__', realpath(__DIR__ . '/../test_files') . '/');
+        }
+        if (!defined('__OBRAY_APP_NAME__')) {
+            define('__OBRAY_APP_NAME__', 'App');
+        }
+        if (!defined('__APP__')) {
+            define('__APP__', 'Obray');
+        }
+        if (!defined('__OBRAY_DATABASE_HOST__')) {
+            define('__OBRAY_DATABASE_HOST__', 'mysql');
+        }
+        if (!defined('__OBRAY_DATABASE_NAME__')) {
+            define('__OBRAY_DATABASE_NAME__', 'obray');
+        }
+        if (!defined('__OBRAY_DATABASE_USERNAME__')) {
+            define('__OBRAY_DATABASE_USERNAME__', 'root');
+        }
+        if (!defined('__OBRAY_DATABASE_PASSWORD__')) {
+            define('__OBRAY_DATABASE_PASSWORD__', 'root');
+        }
+        if (!defined('__LOGS__')) {
+            define('__LOGS__', realpath(__DIR__ . '/../logs') . '/');
+        }
 
         OObject::$handleExceptions = true;
-	}
+    }
 
-	protected function tearDown(): void
-	{
-		if (class_exists(OUsers::class)) {
-			OUsers::$shouldAuthenticate = true;
-		}
+    protected function setUp(): void
+    {
+        parent::setUp();
 
-		parent::tearDown();
-	}
+        $this->router = new OObject();
+        $this->router->setOutput(new NullOutput());
+        $this->router::setContainerSingleton(null);
+    }
+
+    protected function tearDown(): void
+    {
+        if (class_exists(OUsers::class)) {
+            OUsers::$shouldAuthenticate = true;
+        }
+
+        parent::tearDown();
+    }
 
 
-	protected function assertNotError(?OObject $object = null)
-	{
-		$object = $object ?? $this->router;
+    protected function assertNotError(?OObject $object = null)
+    {
+        $object = $object ?? $this->router;
 
-		$errors = json_encode($object->errors ?? []);
+        $errors = json_encode($object->errors ?? []);
 
-		$this->assertFalse($object->isError(), $errors);
-	}
+        $this->assertFalse($object->isError(), $errors);
+    }
 
-	protected function assertError(?OObject $object = null, array $errors = [])
-	{
-		$object = $object ?? $this->router;
+    protected function assertError(?OObject $object = null, array $errors = [])
+    {
+        $object = $object ?? $this->router;
 
-		$this->assertTrue($object->isError(), empty($object->errors) ? 'No error reported' : json_encode($object->errors));
+        $this->assertTrue(
+            $object->isError(),
+            empty($object->errors) ? 'No error reported' : json_encode($object->errors)
+        );
 
-		if (!empty($errors)) {
-			$this->assertEqualsCanonicalizing($errors, $object->errors ?? []);
-		}
-	}
+        if (!empty($errors)) {
+            $this->assertEqualsCanonicalizing($errors, $object->errors ?? []);
+        }
+    }
 
-	protected function route(string $path, array $params = [], bool $direct = false)
-	{
-		return $this->router->route($path, $params, $direct);
-	}
+    protected function route(string $path, array $params = [], bool $direct = false)
+    {
+        return $this->router->route($path, $params, $direct);
+    }
 }
