acd68c832c6fdf689df5914f84e1ab59bb33357e
James Funk <piurafunk@users.noreply.github.com>
2024-09-12 08:35:26 -0600

    Formatting

--- tests/FakesAuthentication.php
+++ tests/FakesAuthentication.php
@@ -6,17 +6,17 @@ use stdClass;
 
 trait FakesAuthentication
 {
-	protected function authenticate()
-	{
-		global $_SESSION;
-		$_SESSION['ouser'] = new stdClass();
+    protected function authenticate()
+    {
+        global $_SESSION;
+        $_SESSION['ouser'] = new stdClass();
         $_SESSION['ouser']->ouser_id = 1;
         $_SESSION['ouser']->ouser_permission_level = 1;
-	}
+    }
 
-	protected function unauthenticate()
-	{
-		global $_SESSION;
-		unset($_SESSION['ouser']);
-	}
+    protected function unauthenticate()
+    {
+        global $_SESSION;
+        unset($_SESSION['ouser']);
+    }
 }
